832d7188b6631a661eedbd7a7795e3fe7ec946dc
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2025-01-19 14:20:25 +0530

    Update temp.js

---
+++ temp.js
@@ -0,0 +1,30 @@
+
+
+
+db.createCollection("nonfiction",
+
+
+    {
+
+        validator:{
+
+            $jsonSchema:{
+                required:['name','price'],
+                properties:{
+                    name:{
+                        bsonType:'string',
+                        description:'must be a string and required'
+                    },
+                    price:{
+                         bsonType:'number',
+                        description:'must be a number and required'
+                    }
+                }
+            }
+            
+        },
+        validationAction:'error'
+    }
+
+    
+)
